ad10ebf52c26957e47704f3eef3b7f2cd41f373d
lilychaco <lilychaco@gmail.com>
2024-12-13 17:36:29 +0900

    header refresh消した

--- CodeUps-WP-divingsite/header.php
+++ CodeUps-WP-divingsite/header.php
@@ -23,9 +23,7 @@
 	<!-- アイコン -->
 	<link rel="icon" href="http://ninikokoji.com/diving-lily/wp-content/uploads/2024/11/iriomote512.png">
 
-	<?php if (is_404()) : ?>
-	<meta http-equiv="refresh" content=" 3; url=<?php echo esc_url(home_url('/')); ?>">
-	<?php endif; ?>
+
 	<?php wp_head(); ?>
 </head>
 
