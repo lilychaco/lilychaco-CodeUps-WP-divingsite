1d1e834795e3613ef3de22909f14516380ad3aa0
lilychaco <lilychaco@gmail.com>
2024-12-16 08:13:22 +0900

    グループタイトルを動的出力

--- CodeUps-WP-divingsite/page-price.php
+++ CodeUps-WP-divingsite/page-price.php
@@ -17,10 +17,13 @@
 		<?php
 		// グループ外の単一フィールド「license_title」を取得
 			$license_title = SCF::get('license_title');
-				// グループ外の単一フィールド「fundiving_title」を取得
-			$license_title = SCF::get('license_title');
-				// Smart Custom Fields (SCF) を使って、'license_pricelist' グループを取得します。
+
+			// Smart Custom Fields (SCF) を使って、'license_pricelist' グループを取得します。
 			$license_pricelist = SCF::get('license_pricelist');
+				?>
+		<?php
+			// 取得したデータが空でないかをチェックします。
+			if (!empty($license_pricelist)) :
 				?>
 		<div class="page-price__item page-price-list fish">
 			<div class="page-price-list__title">
@@ -33,10 +36,7 @@
 					</p>
 				</div>
 			</div>
-			<?php
-			// 取得したデータが空でないかをチェックします。
-			if (!empty($license_pricelist)) :
-				?>
+
 			<ul class="page-price-list__items">
 				<?php
         // 繰り返しフィールドの各アイテムをループで処理します。
@@ -65,6 +65,8 @@
 
 
 		<?php
+				// グループ外の単一フィールド「experience_title」を取得
+			$experience_title = SCF::get('experience_title');
 				// Smart Custom Fields (SCF) を使って、'experience_pricelist' グループを取得します。
 				$experience_pricelist = SCF::get('experience_pricelist');
 
@@ -78,7 +80,7 @@
 					<div class="page-price-list__icon">
 						<img src="<?php echo get_theme_file_uri(); ?>/assets/images/icon01.png" alt="" />
 					</div>
-					<p>体験ダイビング</p>
+					<p><?= esc_html($experience_title ?: 'ダイビング'); ?></p>
 				</div>
 			</div>
 			<ul class="page-price-list__items">
@@ -107,7 +109,7 @@
 			endif;
 			?>
 
-		<!-- <?php
+		<?php
 		// グループ外の単一フィールド「fundiving_title」を取得
 			$fundiving_title = SCF::get('fundiving_title');
 			// Smart Custom Fields (SCF) を使って、'fundiving_pricelist' グループを取得します。
@@ -115,10 +117,8 @@
 
 			// 取得したデータが空でない場合にのみ HTML を出力します。
 			if (!empty($fundiving_pricelist)) :
-		?> -->
-
-
-		<!-- <div class="page-price__item page-price-list">
+		?>
+		<div class="page-price__item page-price-list">
 			<div class="page-price-list__title">
 				<div class="page-price-list__heading">
 					<div class="page-price-list__icon">
@@ -126,31 +126,32 @@
 					</div>
 					<p><?= esc_html($fundiving_title ?: 'ダイビング'); ?></p>
 				</div>
-			</div> -->
-		<!-- <ul class="page-price-list__items">
+			</div>
+			<ul class="page-price-list__items">
 				<?php
             // 取得した繰り返しフィールドの各アイテムをループで処理します。
             foreach ($fundiving_pricelist as $item) :
                 // 'fundiving_name' フィールドの値を取得して出力します。
-                if (!empty($item['fundiving_name'])) : ?> -->
-		<!-- <li class="page-price-list__item">
-			<p class="page-price-list__name"><?php echo nl2br(esc_html($item['fundiving_name'])); ?></p>
-			<?php
-                        // 'fundiving_price' フィールドが存在する場合は値を出力します。
-                        if (!empty($item['fundiving_price'])) : ?>
-			<p class="page-price-list__number"><?php echo esc_html($item['fundiving_price']); ?></p>
-			<?php endif; ?>
-		</li> -->
-		<!-- <?php endif;
+                if (!empty($item['fundiving_name'])) : ?>
+				<li class="page-price-list__item">
+					<p class="page-price-list__name"><?php echo nl2br(esc_html($item['fundiving_name'])); ?></p>
+					<?php
+              // 'fundiving_price' フィールドが存在する場合は値を出力します。
+              if (!empty($item['fundiving_price'])) : ?>
+					<p class="page-price-list__number"><?php echo esc_html($item['fundiving_price']); ?></p>
+					<?php endif; ?>
+				</li>
+				<?php endif;
             endforeach; ?>
-		</ul>
-
-	</div> -->
-		<!-- <?php
-	 	endif; ?> -->
+			</ul>
+		</div>
+		<?php
+	 	endif; ?>
 
 
 		<?php
+		// グループ外の単一フィールド「specialdiving_title」を取得
+			$specialdiving_title = SCF::get('specialdiving_title');
 				// Smart Custom Fields (SCF) を使って、'specialdiving_pricelist' グループを取得します。
 				$specialdiving_pricelist = SCF::get('specialdiving_pricelist');
 
@@ -164,7 +165,9 @@
 					<div class="page-price-list__icon">
 						<img src="<?php echo get_theme_file_uri(); ?>/assets/images/icon01.png" alt="" />
 					</div>
-					<p>スペシャルダイビング</p>
+					<p>
+					<p><?= esc_html($specialdiving_title ?: 'ダイビング'); ?></p>
+					</p>
 				</div>
 			</div>
 			<ul class="page-price-list__items">
@@ -199,9 +202,6 @@
 	</div>
 </div>
 
-<!-- <div class="contact-wrapper"> -->
-<!-- <?php get_template_part('common/contact') ?> -->
-<!-- </div> -->
 
 
 <?php get_footer(); ?>
